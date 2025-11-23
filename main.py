c106ae3b2aba242f2121b4653d07144fbfd3cad0
Daniele Tarek Iaisy <iaisy.danieletarek@gmail.com>
2025-11-23 13:09:57 +0100

    included entrypoint script
    included entrypoint script

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import sys
-from profiles import alpha_beta_basic
+import torch
+from profiles import mcts_fixed_model
 from client import play_game
+from network.model import TablutNet
 from tablut import Player
 
 
-if len(sys.argv) != 3:
-    print("Usage: python main.py <player> <server ip>")
+if len(sys.argv) != 4:
+    print("Usage: python main.py <player> <timeout> <server ip>")
     sys.exit(1)
 
 player_input = sys.argv[1].lower()
-server_ip = sys.argv[2]
+timeout = int(sys.argv[2])
+server_ip = sys.argv[3]
 
 if player_input == "white":
     player = Player.WHITE
@@ -20,5 +23,10 @@ else:
         f"Invalid player. Must be either 'white' or 'black', not {player_input}."
     )
 
-search = alpha_beta_basic(4, 30)
-play_game(player, "MyPythonBot", server_ip, search)
+# search = alpha_beta_basic(4, 30)
+device = torch.device("cpu")
+model = TablutNet().to(device)
+checkpoint_path = "checkpoints/final_checkpoint.pth"
+model.load_state_dict(torch.load(checkpoint_path))
+search = mcts_fixed_model(model, 20, timeout - 5)
+play_game(player, "Tulbat", server_ip, search)
